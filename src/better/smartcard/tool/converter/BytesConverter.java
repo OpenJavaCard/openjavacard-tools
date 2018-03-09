3ad3d71da64a1dbc6e081f605afbc6de30e8e572
Ingo Albrecht <prom@berlin.ccc.de>
2018-03-09 21:36:38 +0100

    Use the new converter

--- src/better/smartcard/tool/converter/BytesConverter.java
+++ src/better/smartcard/tool/converter/BytesConverter.java
@@ -6,7 +6,6 @@ import com.beust.jcommander.IStringConverter;
 public class BytesConverter implements IStringConverter<byte[]> {
     @Override
     public byte[] convert(String value) {
-        System.out.println("BYTECONVERT: " + value);
         return HexUtil.hexToBytes(value);
     }
 }
