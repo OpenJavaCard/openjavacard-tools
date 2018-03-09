7005e57a012f3a2b44f052cbeb9241e364e871aa
Ingo Albrecht <prom@berlin.ccc.de>
2018-03-09 21:05:45 +0100

    Better object presence check in gp-delete

--- src/better/smartcard/commands/GPDelete.java
+++ src/better/smartcard/commands/GPDelete.java
@@ -3,6 +3,7 @@ package better.smartcard.commands;
 import better.smartcard.gp.GPCard;
 import better.smartcard.gp.GPContext;
 import better.smartcard.gp.GPIssuerDomain;
+import better.smartcard.gp.GPRegistry;
 import better.smartcard.util.AID;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
@@ -24,6 +25,12 @@ public class GPDelete extends GPCommand {
     private boolean related = false;
 
     @Parameter(
+            names = "--present",
+            description = "Fail when objects don't exist"
+    )
+    private boolean present = false;
+
+    @Parameter(
             description = "AIDs of objects to delete",
             required = true
     )
@@ -36,6 +43,22 @@ public class GPDelete extends GPCommand {
     @Override
     protected void performOperation(GPContext context, GPCard card) throws CardException {
         PrintStream os = System.out;
+        // check presence
+        GPRegistry reg = card.getRegistry();
+        for (AID aid : objectAIDs) {
+            GPRegistry.Entry entry = reg.findAppletOrPackage(aid);
+            if(entry != null) {
+                os.println("Object " + aid + ": " + entry);
+            } else {
+                if(present) {
+                    throw new CardException("Object " + aid + " is not present on the card");
+                } else {
+                    os.println("Object " + aid + " is not present");
+                    continue;
+                }
+            }
+        }
+        // perform deletions
         GPIssuerDomain isd = card.getIssuerDomain();
         for (AID aid : objectAIDs) {
             os.println("Deleting object " + aid + (related?" and related":""));
