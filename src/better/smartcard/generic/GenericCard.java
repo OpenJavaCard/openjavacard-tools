3329d86df9dc243fad5bc8ae4178ec2237712b8e
Ingo Albrecht <prom@berlin.ccc.de>
2018-03-18 17:28:33 +0100

    Some extra methods for GenericCard

--- src/better/smartcard/generic/GenericCard.java
+++ src/better/smartcard/generic/GenericCard.java
@@ -23,6 +23,10 @@ public class GenericCard {
         mCard = null;
     }
 
+    public CardTerminal getTerminal() {
+        return mTerminal;
+    }
+
     public boolean isConnected() {
         return mCard != null;
     }
@@ -38,6 +42,31 @@ public class GenericCard {
     public void connect() throws CardException {
         mCard = mTerminal.connect("*");
         mBasic = mCard.getBasicChannel();
+    }
+
+    public void reconnect(boolean reset) throws CardException {
+        mCard.disconnect(reset);
+        connect();
+    }
+
+    public void disconnect() {
+        try {
+            mCard.disconnect(false);
+            mCard = null;
+        } catch (CardException e) {
+        }
+    }
+
+    public ResponseAPDU performSelectByName(byte[] name, boolean first) throws CardException {
+        byte p2 = first ? ISO7816.SELECT_P2_FIRST_OR_ONLY : ISO7816.SELECT_P2_NEXT;
+        return performSelect(ISO7816.SELECT_P1_BY_NAME, p2, name);
+    }
+
+    private ResponseAPDU performSelect(byte p1, byte p2, byte[] data) throws CardException {
+        CommandAPDU scapdu = APDUUtil.buildCommand(
+                ISO7816.CLA_ISO7816, ISO7816.INS_SELECT,
+                p1, p2, data);
+        return transmitAndCheck(scapdu);
     }
 
     public ResponseAPDU transmit(CommandAPDU command) throws CardException {
