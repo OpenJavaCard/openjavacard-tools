4618e9c79871e4eb67e363c6b03d765202d633d8
Ingo Albrecht <prom@berlin.ccc.de>
2018-03-20 17:28:56 +0100

    Updated run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 home=`dirname $0`
-java -Dorg.slf4j.simpleLogger.defaultLogLevel=INFO -jar $home/build/jar/javacard-tools-plain.jar $@
+java -Dorg.slf4j.simpleLogger.defaultLogLevel=INFO -jar $home/build/jar/openjavacard-tools-plain.jar $@
