1a6fb231869e96fde77f40460909522967bbacb1
Ingo Albrecht <prom@berlin.ccc.de>
2018-04-04 19:10:14 +0200

    Updated run scripts

--- run-debug.sh
+++ run-debug.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 home=`dirname $0`
-java -Dorg.slf4j.simpleLogger.defaultLogLevel=DEBUG -jar $home/build/jar/openjavacard-tools-plain.jar $@
+java -Dorg.slf4j.simpleLogger.defaultLogLevel=DEBUG -jar $home/build/jar/openjavacard-tools-0.1-SNAPSHOT-fat.jar $@
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 home=`dirname $0`
-java -Dorg.slf4j.simpleLogger.defaultLogLevel=INFO -jar $home/build/jar/openjavacard-tools-plain.jar $@
+java -Dorg.slf4j.simpleLogger.defaultLogLevel=INFO -jar $home/build/jar/openjavacard-tools-0.1-SNAPSHOT-fat.jar $@
