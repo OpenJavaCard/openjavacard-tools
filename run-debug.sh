727e61ff4dc3c10fb49c98c8ca1b8797301925c9
Ingo Albrecht <prom@berlin.ccc.de>
2018-05-19 17:14:31 +0200

    Properly quote arguments

--- run-debug.sh
+++ run-debug.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 home=`dirname $0`
-java -Dorg.slf4j.simpleLogger.defaultLogLevel=DEBUG -jar $home/tool/build/jar/openjavacard-tool-0.1-SNAPSHOT-fat.jar $@
+java -Dorg.slf4j.simpleLogger.defaultLogLevel=DEBUG -jar $home/tool/build/jar/openjavacard-tool-0.1-SNAPSHOT-fat.jar "$@"
