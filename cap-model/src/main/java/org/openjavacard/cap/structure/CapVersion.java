009aa547f4ecdf9e0b5f64922d74799ccd91b625
Ingo Albrecht <prom@berlin.ccc.de>
2018-11-09 15:48:38 +0100

    typo

--- cap-model/src/main/java/org/openjavacard/cap/structure/CapVersion.java
+++ cap-model/src/main/java/org/openjavacard/cap/structure/CapVersion.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 public class CapVersion {
 
     public static final CapVersion CAP21 = new CapVersion(2, 1);
-    public static final CapVersion CAP22 = new CapVersion(2, 1);
+    public static final CapVersion CAP22 = new CapVersion(2, 2);
 
     public final int major;
     public final int minor;
