654bc0ce41b0996e284ce566d0a780fd433bf82a
Ingo Albrecht <prom@berlin.ccc.de>
2018-12-19 17:32:49 +0100

    More CAP parsing

---
+++ cap-model/src/main/java/org/openjavacard/cap/structure/CapArrayInit.java
@@ -0,0 +1,15 @@
+package org.openjavacard.cap.structure;
+
+import org.openjavacard.cap.base.CapStructure;
+import org.openjavacard.cap.base.CapStructureReader;
+
+import java.io.IOException;
+
+public class CapArrayInit extends CapStructure {
+
+    @Override
+    public void read(CapStructureReader reader) throws IOException {
+
+    }
+
+}
