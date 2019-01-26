c5765ac7f61aa3af38fb8daf1e32fef22ee57576
Ingo Albrecht <prom@berlin.ccc.de>
2019-01-26 12:09:13 +0100

    GPKeySet: refactored diversification

--- globalplatform/src/main/java/org/openjavacard/gp/keys/GPKeySet.java
+++ globalplatform/src/main/java/org/openjavacard/gp/keys/GPKeySet.java
@@ -19,7 +19,7 @@
 
 package org.openjavacard.gp.keys;
 
-import org.openjavacard.gp.crypto.GPCrypto;
+import org.openjavacard.gp.scp.SCPDiversification;
 import org.openjavacard.util.HexUtil;
 
 import java.util.ArrayList;
@@ -196,53 +196,24 @@ public class GPKeySet {
         }
         String diversifiedName = mName + "-" + diversification.name() + ":" + HexUtil.bytesToHex(diversificationData);
         GPKeySet diversifiedKeys = new GPKeySet(diversifiedName, mKeyVersion, diversification);
-        switch(diversification) {
-            case EMV:
-                for(GPKeyUsage type: DIVERSIFICATION_KEYS) {
-                    GPKey key = getKeyByUsage(type);
-                    if(key != null) {
-                        diversifiedKeys.putKey(diversifyKeyEMV(type, getKeyByUsage(type), diversificationData));
-                    }
+        for(GPKeyUsage type: DIVERSIFICATION_KEYS) {
+            GPKey key = getKeyByUsage(type);
+            if(key != null) {
+                GPKey diversifiedKey;
+                switch (diversification) {
+                    case EMV:
+                        diversifiedKey = SCPDiversification.diversifyKeyEMV(type, key, diversificationData);
+                        break;
+                    case VISA2:
+                        diversifiedKey = SCPDiversification.diversifyKeyVisa2(type, key, diversificationData);
+                        break;
+                    default:
+                        throw new RuntimeException("Unsupported diversification " + diversification);
                 }
-                break;
-            case VISA2:
-                for(GPKeyUsage type: DIVERSIFICATION_KEYS) {
-                    GPKey key = getKeyByUsage(type);
-                    if(key != null) {
-                        diversifiedKeys.putKey(diversifyKeyVisa2(type, getKeyByUsage(type), diversificationData));
-                    }
-                }
-                break;
-            default:
-                throw new UnsupportedOperationException("Diversification " + diversification + " not supported");
+                diversifiedKeys.putKey(diversifiedKey);
+            }
         }
         return diversifiedKeys;
-    }
-
-    private GPKey diversifyKeyEMV(GPKeyUsage usage, GPKey key, byte[] diversificationData) {
-        byte[] data = new byte[16];
-        System.arraycopy(diversificationData, 4, data, 0, 6);
-        data[6] = (byte)0xF0;
-        data[7] = usage.diversifyId;
-        System.arraycopy(diversificationData, 4, data, 8, 6);
-        data[14] = (byte)0x0F;
-        data[15] = usage.diversifyId;
-        byte[] dKey = GPCrypto.enc_3des_ecb(key, data);
-        return new GPKey(key.getId(), usage, key.getCipher(), dKey);
-    }
-
-    private GPKey diversifyKeyVisa2(GPKeyUsage usage, GPKey key, byte[] diversificationData) {
-        byte[] data = new byte[16];
-        System.arraycopy(diversificationData, 0, data, 0, 2);
-        System.arraycopy(diversificationData, 4, data, 2, 4);
-        data[6] = (byte)0xF0;
-        data[7] = usage.diversifyId;
-        System.arraycopy(diversificationData, 0, data, 8, 2);
-        System.arraycopy(diversificationData, 4, data, 10, 4);
-        data[14] = (byte)0x0F;
-        data[15] = usage.diversifyId;
-        byte[] dKey = GPCrypto.enc_3des_ecb(key, data);
-        return new GPKey(key.getId(), usage, key.getCipher(), dKey);
     }
 
     public String toString() {
---
+++ globalplatform/src/main/java/org/openjavacard/gp/scp/SCPDiversification.java
@@ -0,0 +1,57 @@
+/*
+ * openjavacard-tools: Development tools for JavaCard
+ * Copyright (C) 2019 Ingo Albrecht <copyright@promovicz.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+package org.openjavacard.gp.scp;
+
+import org.openjavacard.gp.crypto.GPCrypto;
+import org.openjavacard.gp.keys.GPKey;
+import org.openjavacard.gp.keys.GPKeyDiversification;
+import org.openjavacard.gp.keys.GPKeySet;
+import org.openjavacard.gp.keys.GPKeyUsage;
+import org.openjavacard.util.HexUtil;
+
+public class SCPDiversification {
+
+    public static final GPKey diversifyKeyEMV(GPKeyUsage usage, GPKey key, byte[] diversificationData) {
+        byte[] data = new byte[16];
+        System.arraycopy(diversificationData, 4, data, 0, 6);
+        data[6] = (byte)0xF0;
+        data[7] = usage.diversifyId;
+        System.arraycopy(diversificationData, 4, data, 8, 6);
+        data[14] = (byte)0x0F;
+        data[15] = usage.diversifyId;
+        byte[] dKey = GPCrypto.enc_3des_ecb(key, data);
+        return new GPKey(key.getId(), usage, key.getCipher(), dKey);
+    }
+
+    public static final GPKey diversifyKeyVisa2(GPKeyUsage usage, GPKey key, byte[] diversificationData) {
+        byte[] data = new byte[16];
+        System.arraycopy(diversificationData, 0, data, 0, 2);
+        System.arraycopy(diversificationData, 4, data, 2, 4);
+        data[6] = (byte)0xF0;
+        data[7] = usage.diversifyId;
+        System.arraycopy(diversificationData, 0, data, 8, 2);
+        System.arraycopy(diversificationData, 4, data, 10, 4);
+        data[14] = (byte)0x0F;
+        data[15] = usage.diversifyId;
+        byte[] dKey = GPCrypto.enc_3des_ecb(key, data);
+        return new GPKey(key.getId(), usage, key.getCipher(), dKey);
+    }
+
+}
