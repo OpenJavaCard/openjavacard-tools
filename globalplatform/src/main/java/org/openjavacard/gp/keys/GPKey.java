d7fb8399dff15dcbf1b523535300f27f9c107421
Ingo Albrecht <prom@berlin.ccc.de>
2019-02-14 12:59:28 +0100

    GPKey: made master key public

--- globalplatform/src/main/java/org/openjavacard/gp/keys/GPKey.java
+++ globalplatform/src/main/java/org/openjavacard/gp/keys/GPKey.java
@@ -30,7 +30,7 @@ public class GPKey {
     /**
      * GlobalPlatform default master key
      */
-    private static final byte[] GLOBALPLATFORM_MASTER_SECRET = {
+    public static final byte[] GLOBALPLATFORM_MASTER_SECRET = {
             0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47,
             0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F
     };
