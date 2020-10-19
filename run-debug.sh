add034d27abd953ae21a395cf4cd391e09ed0dcf
Ingo Albrecht <commit@promovicz.org>
2020-10-19 13:19:12 +0200

    Gradle update fixes

--- run-debug.sh
+++ run-debug.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 home=`dirname $0`
-java -Dorg.slf4j.simpleLogger.defaultLogLevel=DEBUG -jar $home/tool/build/jar/openjavacard-tool-0.1-SNAPSHOT-fat.jar "$@"
+java -Dorg.slf4j.simpleLogger.defaultLogLevel=DEBUG -jar $home/tool/build/libs/openjavacard-tool-fat-0.1-SNAPSHOT.jar "$@"
